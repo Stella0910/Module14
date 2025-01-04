8ea706fb51353feef43f1c4633d072826b2a094d
Stella0910 <aniretake0910@gmail.com>
2025-01-04 21:03:40 +0300

    Add files via upload

--- module_14_1.py
+++ module_14_1.py
@@ -13,13 +13,11 @@ balance INTEGER NOT NULL
 ''')
 for i in range(1, 11):
     cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
-                   (f"User{i}", f"example{i}@gmail.com", i*10, 1000))
+                   (f"User{i}", f"example{i}@gmail.com", i * 10, 1000))
 for i in range(1, 11, 2):
-    cursor.execute('UPDATE Users SET balance = ? WHERE id = ?',
-                   (500, f'{i}'))
+    cursor.execute('UPDATE Users SET balance = ? WHERE id = ?', (500, f'{i}'))
 for i in range(1, 11, 3):
-    cursor.execute('DELETE FROM Users WHERE id = ?',
-                   (f'{i}',))
+    cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}',))
 cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
 users = cursor.fetchall()
 for user in users:
